d6d8434d21f38927be6d3255b783a85d8cdc4901
Deveshree Ghanshyam Bhakkad <bhakkaddeveshree@gmail.com>
2026-01-13 22:22:28 +0530

    updated UI

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "BlogRanker is running 🚀"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
