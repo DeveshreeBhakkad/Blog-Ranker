12e7b08853c0dc709ddc72457da1664c0f20f6aa
Deveshree Ghanshyam Bhakkad <bhakkaddeveshree@gmail.com>
2026-01-14 15:47:52 +0530

    Finalize ReadFlow UI with dark theme and guided reading layout

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
 from flask import Flask, render_template, request
-from recommender import rank_blogs
-from search import search_blogs   # your web search file
+from search import search_blogs
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    grouped_results = None
+    results = None
 
     if request.method == "POST":
         query = request.form.get("query")
-
         blogs = search_blogs(query)
-        ranked = rank_blogs(query, blogs)
 
-        grouped_results = {
-            "Beginner": [],
-            "Intermediate": [],
-            "Advanced": []
+        # Group blogs by level
+        grouped = {
+            "beginner": [],
+            "intermediate": [],
+            "advanced": []
         }
 
-        for blog in ranked:
-            grouped_results[blog["level"]].append(blog)
+        for blog in blogs:
+            level = blog.get("level", "intermediate")
+            grouped[level].append(blog)
 
-    return render_template("index.html", results=grouped_results)
+        results = grouped
+
+    return render_template("index.html", results=results)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
